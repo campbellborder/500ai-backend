1aa41e0aba5b04fbcd1915a7a6cb3001ab42670c
Campbell Border <campbellborder64@gmail.com>
2023-09-01 18:43:34 +1000

    deck and username showing in play

--- classes/game.py
+++ classes/game.py
@@ -5,7 +5,6 @@ from fastapi import (
 from classes.player import Player
 from rlcard.games.five_hundred.game import FiveHundredGame
 
-
 async def create_game(gamecode, username, ws):
     game = Game(gamecode, username, ws)
     await game._init()
@@ -44,16 +43,18 @@ class Game:
 
     async def _broadcast_state(self):
         for player in self.players:
-            await player.ws.send_text(json.dumps(self.get_state_message(player.username)))
+            if player.is_human():
+                await player.send(self.get_state_message(player.username))
     
     async def _broadcast_alert(self, status, username):
         for player in self.players:
-            message = {"type": "alert", "status": status, "username": username}
-            if status == "new-host":
-                message["you"] = player.username == username
-            if status == "player-joined" and player.username == username:
-                continue
-            await player.ws.send_text(json.dumps(message))
+            if player.is_human():
+                message = {"type": "alert", "status": status, "username": username}
+                if status == "new-host":
+                    message["you"] = player.username == username
+                if status == "player-joined" and player.username == username:
+                    continue
+                await player.send(message)
 
     async def update(self, username, update):
         if update["phase"] == self.phase:
@@ -62,21 +63,38 @@ class Game:
                 case "move-position":
                     self._get_player(username).position = action["position"]
                 case "start-game":
-                    self.phase = "play"
-                    self._game.init_game()
+                    self._start_game()
 
         await self._broadcast_state()
+
+    def _start_game(self):
+        self.phase = "play"
+        _, current_player_id = self._game.init_game()
+        self.current_position = Game.positions[current_player_id]
+        for position in set(Game.positions) - set(self._taken_positions()):
+            self.players.append(Player("robot", None, position))
+
+        self._order_players()
+
+
+    def _num_human_players(self):
+        return sum([player.is_human() for player in self.players])
+    
+    def _order_players(self):
+        self.players.sort(key=lambda player: Game.positions.index(player.position))
 
     async def player_disconnect(self, username):
         
         player = self._get_player(username)
+        position = player.position
         self.players.remove(player)
 
-        if not len(self.players): 
+        if not self._num_human_players(): 
             # Only player has left, destroy game
             self.over = True
             for player in self.players:
-                await player.ws.close()
+                if player.is_human():
+                  await player.ws.close()
             return
 
         await self._broadcast_alert("player-left", player.username)
@@ -87,8 +105,8 @@ class Game:
             await self._broadcast_alert("new-host", self.players[0].username)
 
         if self.phase == "play":
-            # Substitute AI for player
-            raise NotImplementedError
+            self.players.append("robot", None, position)
+            self._order_players()
 
         await self._broadcast_state()
         
@@ -98,12 +116,39 @@ class Game:
         message["type"] = "state"
         message["phase"] = self.phase
         message["gamecode"] = self.gamecode
+        message["players"] = [Player.get_state_repr(player, username) for player in self.players]
+        
         if self.phase == "setup":
-            message["players"] = [Player.get_state_repr(player, username) for player in self.players]
             for position in set(Game.positions) - set(self._taken_positions()):
                 message["players"].append(Player.get_state_repr(None, None, position))
             
         elif self.phase == "play":
-            pass
+            state = self._game.get_perfect_information()
+            print(state)
+            message["round_phase"] = state["round_phase"]
+            message["scores"] = state["scores"]
+            if state["round_phase"] == "bid":
+                message["last_bid"] = state["last_bid"]
+                for i, player in enumerate(message["players"]):
+                    if player["you"]:
+                        player["hand"] = state["hands"][i]
+                    player["bids"] = state["bids"][i]
+                    player["num_cards"] = len(state["hands"][i])
+            
+            elif state["round_phase"] == "discard":
+                # current player hand
+                pass
+            elif state["round_phase"] == "play":
+                # current player hand
+                # declarers hand if OM and 1 trick been played
+                pass
+            else:
+                # over?
+                pass
+            
+                # valid actions
+
+            print(message)
 
         return message
+  
--- classes/player.py
+++ classes/player.py
@@ -1,10 +1,20 @@
 from fastapi import (
     WebSocket
 )
+import json
+from rlcard.games.five_hundred.utils.five_hundred_card import FiveHundredCard
+from rlcard.games.five_hundred.utils.move import CallMove
+
+def custom_serializer(obj):
+    if isinstance(obj, FiveHundredCard):
+        return obj.__repr__()
+    if isinstance(obj, CallMove):
+        return obj.__repr__()
+    raise TypeError(f"Type {type(obj).__name__} is not serializable")
 
 class Player():
 
-    def __init__(self, username: str, ws: WebSocket, position: str, host: bool = False):
+    def __init__(self, username: str, ws: WebSocket | None, position: str, host: bool = False):
         self.username = username
         self.ws = ws
         self.position = position
@@ -16,6 +26,14 @@ class Player():
                 "position": position,
                 "type": "empty"
             }
+        elif player.ws is None:
+            return {
+                "position": player.position,
+                "type": "ai",
+                "username": player.username,
+                "host": False,
+                "you": False
+            }
         return {
             "position": player.position,
             "type": "human",
@@ -23,3 +41,10 @@ class Player():
             "host": player.host,
             "you": player.username == you
         }
+
+    def is_human(self):
+        return self.ws is not None
+    
+    async def send(self, data):
+        if self.is_human():
+            await self.ws.send_text(json.dumps(data, default=custom_serializer))
